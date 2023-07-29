6ddc9993e4cd6aec4839bc1a2d6fc140af4e4779
Crowdbotics <team@crowdbotics.com>
2023-07-29 17:54:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-mountain-42515",
+  url: "https://icy-mountain-42515.botics.co",
+  api: "https://icy-mountain-42515.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
